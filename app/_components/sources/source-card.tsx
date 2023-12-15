cd2ff5298141895e45de6abc695a1e738e971316
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-12-15 18:03:15 +0530

    Showing dummy resources

---
+++ app/_components/sources/source-card.tsx
@@ -0,0 +1,43 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { StarIcon } from '@radix-ui/react-icons';
+
+function SourceCard() {
+  return (
+    <Card className="w-[200px] sm:w-fit">
+      <CardHeader className="grid items-start gap-4 space-y-0">
+        <div className="space-y-1">
+          <CardTitle className="w-[150px] md:w-[250px] overflow-hidden text-ellipsis whitespace-nowrap">
+            Artificial intelligence - Wikipedia
+          </CardTitle>
+          <CardDescription className="w-[150px] md:w-[250px] overflow-hidden text-ellipsis whitespace-nowrap">
+            Artificial intelligence (AI) is the simulation of human intelligence
+            processes by machines, especially computer systems. Read the full
+            definition.
+          </CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <div className="flex space-x-4 text-sm text-muted-foreground">
+          <div className="flex items-center">
+            <img
+              className="mr-1 h-3 w-3"
+              src="https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://wikipedia.org&size=128"
+            />
+            wikipedia
+          </div>
+          <div className="flex items-center">
+            <StarIcon className="mr-1 h-3 w-3" />1
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default SourceCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 'use client';
-import { getSearchResults } from '@/utils/db';
 import SearchBox from './_components/search-box';
 import { useState } from 'react';
 import QuestionTitle from './_components/question-title';
+import PagesFetched from './_components/sources';
 
 export default function Home() {
   const [loading, setLoading] = useState(false);
 
   return (
-    <div className="w-screen h-screen flex items-center flex-col p-8 max-w-7xl mx-auto px-72">
+    <div className="w-screen h-screen max-w-screen-2xl flex items-center flex-col p-8 overflow-x-hidden mx-auto md:px-72">
       <SearchBox onSubmit={() => {}} />
       <QuestionTitle title="What is AI?" />
       {/* Searched pages */}
+      <PagesFetched />
     </div>
   );
 }
